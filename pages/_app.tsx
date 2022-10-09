76103501fbc191363be753a595977c454849f15e
Amodu Kehinde <amodukehindee@gmail.com>
2022-10-09 12:23:44 +0100

    fix store error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import '../styles/globals.css'
-import {store} from '../redux/store'
+// import {store} from '../redux/store'
 
-import {Provider} from 'react-redux'
+// import {Provider} from 'react-redux'
 
 function MyApp({ Component, pageProps }) {
   return (
 		<>
-			<Provider store={store}>
+			{/* <Provider store={store}> */}
 				<Component {...pageProps} />
-			</Provider>
+			{/* </Provider> */}
 		</>
 	);
 }
